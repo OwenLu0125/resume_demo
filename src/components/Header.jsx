776deaa47b5d0339358c7fd980c0f5d312e79214
OwenLu <gg0125gg@gmail.com>
2024-04-28 11:33:30 +0800

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  background-color: black;
+  color: white;
+  font-size: 18px;
+`;
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: end;
+  max-width: 1280px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 20px 0;
+`;
+const StyledLink = styled.a`
+  padding: 10px 24px;
+  text-wrap: nowrap;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const Header = () => {
+  return (
+    <>
+      <StyledHeader>
+        <StyledNav>
+          <StyledLink href='#' class='link link-icon'>
+            作品集
+            <img src='/images/icon/chevron-down.svg' alt='chevron-down' />
+          </StyledLink>
+          <StyledLink href='#' class='link link-icon'>
+            關於我
+            <img src='/images/icon/chevron-down.svg' alt='chevron-down' />
+          </StyledLink>
+          <StyledLink href='#' class='link link-icon'>
+            工作經驗
+            <img src='/images/icon/chevron-down.svg' alt='chevron-down' />
+          </StyledLink>
+          <StyledLink href='#' class='link link-border'>
+            聯絡我
+          </StyledLink>
+        </StyledNav>
+      </StyledHeader>
+    </>
+  );
+};
+
+export default Header;
